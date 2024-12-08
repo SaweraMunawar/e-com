4c77ea3d7f9da066eba1ec4d138f8a221000f253
SaweraMunawar <saweramunawar99@gmail.com>
2024-12-09 01:02:13 +0500

    Add files via upload

---
+++ src/components/card.ts
@@ -0,0 +1,24 @@
+import { title } from "process";
+
+const arr1 =[
+    { id: 1, 
+      title:"Use the styles", 
+      description:"Styles in Figma mean you don’t have to repetitively enter values for typography, colors and elevations minimising the risk of inconsistencies and streamlining the design process.Within this template you can find color and type styles, and their definitions on the style guide page. To use them simply select them from the Figma style panel as shown below.", 
+      image: ""
+    },
+    { id: 2, 
+        title:"Use the components", 
+        description:"There’s a bunch of pre-configured components for you to use in this free template. Simply go to the Style Guide page and under the components header you can see everything included. Buttons, steppers, filter options and more for you to integrate into your screens and modify to your tastes and requirements.", 
+        image: ""
+    },
+    { id: 3, 
+        title:"Check the layouts", 
+        description:"As well as components and styles we’ve built a bunch of layouts in both mobile and desktop sizes. Play around with replacing and modifying the nested content blocks and to switch device, simply use the content block property ‘device’ to toggle between mobile and desktop.", 
+        image: ""
+    },
+    { id: 4, 
+        title:"Use the content blocks", 
+        description:"We’ve built out a multitude of content blocks for you to use in your landing pages. From hero sections, forms, CTAs to pricing sections. Everything you need to get started building polished, modern landing pages.", 
+        image: ""
+    }
+]
